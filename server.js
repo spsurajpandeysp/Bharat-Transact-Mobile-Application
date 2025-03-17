059e0bca12aab1d6278f535a6bb3be6af80f2e97
Suraj Pandey <surajpandey7493@gmail.com>
2025-03-17 14:30:37 +0530

    verce config changed

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.json())
 
-app.get('/',(req,res)=>{
+app.get('*',(req,res)=>{
   res.send('Welcome to brarat transact')
 })
 app.use('/auth/api/',authRounter);
